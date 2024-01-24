5676241b21ed00acef3b49912e0790602dc82871
nkoro <nkoro@DESKTOP-E4LHFO3>
2024-01-25 02:53:43 +0500

    hello
    hey

--- 1111111/obj/Debug/net7.0-windows/1111111.AssemblyInfo.cs
+++ 1111111/obj/Debug/net7.0-windows/1111111.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1111111")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1dedf77840e3d44517a201ee337708ac4575b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12f3f1d63693902276f4745c74faf51ee4b5f068")]
 [assembly: System.Reflection.AssemblyProductAttribute("1111111")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1111111")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
